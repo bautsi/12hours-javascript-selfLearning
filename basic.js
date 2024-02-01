cb6e1076e5d94c8f0be9ac4c610b88162616074c
Brad <b3r1a3dtw@gmail.com>
2024-02-01 20:34:29 +0800

    first commit

---
+++ basic.js
@@ -0,0 +1,12 @@
+console.log("Hello");
+console.log("I like pizza!");
+console.warn("warnning!");
+console.error("OMGOMG");
+
+// window.alert("This is an alert!");
+// window.alert("I like pizza!");
+
+/* This is a comment */
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I really like pizza!";
